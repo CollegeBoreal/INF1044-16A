9a9e4d077f62f271f7220b0b5916a2288df01a23
Gary Uppiah <Gary Uppiah>
2016-11-08 12:04:05 -0500

    souffrance

--- 5.TESTS/my-dream-app-Gary/src/app/dashboard/ville-list/ville-list.component.ts
+++ 5.TESTS/my-dream-app-Gary/src/app/dashboard/ville-list/ville-list.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ville-list',
-  //templateUrl: './ville-list.component.html',
-  template: 'ville',
+  templateUrl: './ville-list.component.html',
+  //templateUrl: 'ville',
   styleUrls: ['./ville-list.component.css']
 })
 export class VilleListComponent implements OnInit {
