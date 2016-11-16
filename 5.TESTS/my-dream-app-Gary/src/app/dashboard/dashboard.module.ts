82720ecd0f4563f412c8c7f5290dcd46d54323b5
Gary Uppiah <Gary Uppiah>
2016-11-16 12:48:05 -0500

    avancement

--- 5.TESTS/my-dream-app-Gary/src/app/dashboard/dashboard.module.ts
+++ 5.TESTS/my-dream-app-Gary/src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { VilleListComponent } from './ville-list/ville-list.component';
+import { appRouting } from "../app.route";
+ 
+const appRoutes: Routes = [
+  { path: '/ville-list', component: VilleListComponent }
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    appRouting
   ],
   declarations: [DashboardComponent, VilleListComponent]
 })
