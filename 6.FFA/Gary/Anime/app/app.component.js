6f1ca82fff1476dfbef579d2103bb75ced70e165
Jean Olivier Gary Uppiah (Live@EDU) <300096005@mahajunga.borealc.on.ca>
2016-11-16 15:01:40 -0500

    animate mais e lappele anime

---
+++ 6.FFA/Gary/Anime/app/app.component.js
@@ -0,0 +1,28 @@
+"use strict";
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
+        this.courant = false;
+    }
+    AppComponent.prototype.balancer = function () {
+        this.courant = !this.courant;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: "my-app",
+            templateUrl: "app.component.html",
+            animations: [
+                core_1.trigger('etat', [
+                    core_1.state("inactif", core_1.style({ "background-color": "red" })),
+                    core_1.state("actif", core_1.style({ "background-color": "green" })),
+                    core_1.transition("inactif => actif", [core_1.animate("60ms ease-out")]),
+                    core_1.transition("actif => inactif", [core_1.animate("60ms ease-out")]),
+                ])
+            ]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
