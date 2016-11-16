ca5abacb1fb490c7f6177770250d36c1c46cb1be
SetraR <setrar@valiha.com>
2016-11-16 15:01:46 -0500

    Animate

---
+++ 6.FFA/Brice/Animate/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, trigger, state, transition, animate, style } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    templateUrl: "app.component.html",
+    animations: [
+        trigger('etat', [
+            state("inactif", style({ "background-color": "red"})),
+            state("actif", style({ "background-color": "green"})),
+            transition("inactif => actif", [ animate("600ms ease-out")]),
+            transition("actif => inactif", [ animate("600ms ease-out")])
+        ])
+    ]
+})
+export class AppComponent {
+    public courant: boolean;
+
+    constructeur() {
+        this.courant = false;
+    }
+
+    balancer() {
+        this.courant = !this.courant;
+    }
+}
