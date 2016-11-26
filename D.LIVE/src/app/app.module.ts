05f450c8b6a09ec86bafcc208f291fb41e4d059d
Setra <setrar@valiha.com>
2016-11-26 18:40:28 -0500

    Working live

--- D.LIVE/src/app/app.module.ts
+++ D.LIVE/src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { HeroesModule }         from './heroes/heroes.module';
+import { LoginRoutingModule }   from './login-routing.module';
+import { LoginComponent }       from './login.component';
+
+import { DialogService }        from './dialog.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HeroesModule,
+    LoginRoutingModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  providers: [
+    DialogService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
