19bcc77f9fac6a74b87ef85139db3daf64c01726
Julien <julien-liebel@hotmail.com>
2016-12-14 15:00:29 -0500

    :(

--- 4.PROJET/projet-Julien/app/app.routing.ts
+++ 4.PROJET/projet-Julien/app/app.routing.ts
@@ -5,33 +5,33 @@ import { AppComponent } from './app.component';
  import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-//   {
-//     path: '',
-//     redirectTo: '/home',
-//     pathMatch: 'full'
-//   }
-     //{ path: '', redirectTo: "main", pathMatch:"full"}
-    //  { path: 'main', component: AppComponent },
-  {
-  path: '',
-  redirectTo: '/home',
-  pathMatch: 'full'
-},
+// const routes: Routes = [
 // //   {
-// //     path: 'login', component: LoginComponent
+// //     path: '',
+// //     redirectTo: '/home',
+// //     pathMatch: 'full'
 // //   }
-];
+//      //{ path: '', redirectTo: "main", pathMatch:"full"}
+//     //  { path: 'main', component: AppComponent },
+//   {
+//   path: '',
+//   redirectTo: '/home',
+//   pathMatch: 'full'
+// },
+// // //   {
+// // //     path: 'login', component: LoginComponent
+// // //   }
+// ];
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
-            //   { path: '', redirectTo: "Home-page", pathMatch:"full"},
-            //   { path: 'main', component: AppComponent },
-            //   { path: 'Home-page', component: HomeComponent },
-            //   { path: 'Login-page', component: LoginComponent },
+            { path: '', redirectTo: "Home-page", pathMatch:"full"},
+            { path: 'main', component: AppComponent },
+            { path: 'Home-page', component: HomeComponent },
+            { path: 'Login-page', component: LoginComponent },
             
-            routes
+            //routes
         ])
     ],
     exports: [
--- 4.PROJET/projet-Julien/app/home/home-routing.module.js
+++ 4.PROJET/projet-Julien/app/home/home-routing.module.js
@@ -2,6 +2,8 @@
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var home_component_1 = require('./home.component');
+var edit_component_1 = require('./edit.component');
+var add_component_1 = require('./add.component');
 var homeRoutes = 
 // [
 //   {
@@ -19,38 +21,38 @@ var homeRoutes =
 //     ]
 //   }
 // ];
-[
-    {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: home_component_1.HomeComponent,
-        children: [
-            {
-                path: '',
-                component: home_component_1.HomeComponent,
-                children: [
-                    {
-                        path: ':id',
-                        component: home_component_1.HomeComponent
-                    },
-                    {
-                        path: '',
-                        component: home_component_1.HomeComponent
-                    }
-                ]
-            }
-        ]
-    }
-];
 // [
-//   { path: 'Home-page',  component: HomeComponent },
-//   { path: 'Home-page/:id', component: EditComponent },
-//   { path: 'Add-page', component: AddComponent }
+//   {
+//     path: '',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'home',
+//     component: HomeComponent,
+//     children: [
+//       {
+//         path: '',
+//         component: HomeComponent,
+//         children: [
+//           {
+//             path: ':id',
+//             component: HomeComponent
+//           },
+//           {
+//             path: '',
+//             component: HomeComponent
+//           }
+//         ]
+//       }
+//     ]
+//   }
 // ];
+[
+    { path: 'Home-page', component: home_component_1.HomeComponent },
+    { path: 'Home-page/:id', component: edit_component_1.EditComponent },
+    { path: 'Add-page', component: add_component_1.AddComponent }
+];
 var HomeRoutingModule = (function () {
     function HomeRoutingModule() {
     }
